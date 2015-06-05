d87a95210a860b2422766b2051500f774a4ebb24
Juan Chaves <juan.cha63@gmail.com>
2015-06-05 09:57:35 +0200

    Despliegue DB en HeroKu2

--- models/models.js
+++ models/models.js
@@ -32,15 +32,15 @@ var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
 exports.Quiz = Quiz; // Exportar la definicion de la tabla Quiz
 
 // Seuelize.syc() crea e inicializa tabla de preguntas en DB
-sequelize.sync().then(function() {
-	// then(...) ejecuta el namejador una vez creada la tabla
-	Quiz.count().then(function (count) {
+sequelize.sync().success(function() {
+	// Success(...) ejecuta el namejador una vez creada la tabla
+	Quiz.count().success(function (count) {
 		if(count === 0) {  // La tabla se inicializa solo si esta vacia
 			Quiz.create({
 							pregunta: 'Capital de Italia',
 							respuesta: 'Roma'
 						})
-			.then(function() {console.log('Base de datos inicializada')});
+			.success(function() {console.log('Base de datos inicializada')});
 		};
 	});
 });
